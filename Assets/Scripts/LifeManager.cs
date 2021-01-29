fa2b2cadfccf419e44c2c253cd2320f5efb61d90
berna <berna@DESKTOP-6DPAKFC>
2021-01-29 23:42:43 +0100

    fix bug, add item props

---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeManager : MonoBehaviour
+{
+    public int Life = 50;
+
+    [HideInInspector]
+    public int AcctuallyLife;
+
+    private void Start()
+    {
+        AcctuallyLife = Life;
+    }
+
+    public void AddDamage(int damage)
+    {
+        AcctuallyLife -= Life;
+
+        if (AcctuallyLife <= 0)
+            OnDead();
+    }
+
+    void OnDead()
+    {
+        Debug.Log("porcamadonnalaida");
+    }
+}
